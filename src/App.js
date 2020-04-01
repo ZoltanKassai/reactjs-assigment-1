c6517bfdf3143a7dfcb65ee56655c2cb0322aeb2
Zoltan Kassai <zoltan.kassai@mirumagency.com>
2020-04-01 13:05:48 +0200

    code cleaning

--- src/App.js
+++ src/App.js
@@ -38,10 +38,6 @@ class App extends Component {
     return (
       <div className="App">
         <ol>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
-          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
           <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ol>
         <UserInput
